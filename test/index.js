85963e1b9c9a2fd8a8a005ed21684bf741057c4e
Andrei Karpushonak <andrei@avrora.io>
2015-08-02 19:21:27 +0200

    made baseline for clean hinting and coverage

--- test/index.js
+++ test/index.js
@@ -1,9 +1,35 @@
 'use strict';
-var assert = require('assert');
+
+/* global it, before, describe */
+
 var rtbValidator = require('../lib');
+var fs = require('fs');
+var debug = require('debug')('test');
+require('should');
 
 describe('rtb-validator', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+
+  var bidRequest;
+  var bidResponse;
+
+  before(function () {
+    bidRequest = JSON.parse(fs.readFileSync(__dirname + '/data/bid_request.json'));
+    bidResponse = JSON.parse(fs.readFileSync(__dirname + '/data/bid_response.json'));
+  });
+  
+  it('should validate BidRequest', function (done) {
+
+    console.log(bidRequest);
+    bidRequest.should.not.be.equal(null);
+    done();
+
+  });
+
+  it('should validate BidResponse', function (done) {
+
+    console.log(bidResponse);
+    bidRequest.should.not.be.equal(null);
+    done();
+
   });
 });
